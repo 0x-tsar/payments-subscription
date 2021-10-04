9ccd560e6d0b6c30d4bbeb91015261d96b7d2931
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-04 13:05:11 -0300

    adding new live branch

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,4 +1,3 @@
-// const Payments = artifacts.require("Payments");
 const Montly = artifacts.require("Montly");
 
 module.exports = function (deployer) {
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,28 @@
-// const Payments = artifacts.require("Payments");
 const Montly = artifacts.require("Montly");
 
 module.exports = async () => {
-  // const payments = await Payments.deployed();
   const montly = await Montly.deployed();
-  const [account1, account2, _] = await web3.eth.getAccounts();
+  const [account1, _] = await web3.eth.getAccounts();
 
   let COUNTER = 0;
 
-  // let tx = await montly.makePayment({
-  //   from: account1,
-  //   value: web3.utils.toWei("0.1"),
-  // });
-  // console.log(tx);
-
   console.log("Starting node...");
   // const retrievingFunds = await montly.retrievePayment();
-  // console.log(retrievingFunds);
 
-  // setInterval(async () => {
-  //   try {
-  //     console.log("calling..");
+  setInterval(async () => {
+    try {
+      console.log("calling..");
 
-  //     let tx = await montly.makePayment({
-  //       from: account1,
-  //       value: web3.utils.toWei("0.1"),
-  //     });
+      let tx = await montly.makePayment({
+        from: account1,
+        value: web3.utils.toWei("0.1"),
+      });
 
-  //     console.log(tx);
-  //     COUNTER += 1;
-  //     console.log(`payments made: ${COUNTER}`);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, 1000 * 20);
+      console.log(tx);
+      COUNTER += 1;
+      console.log(`payments made: ${COUNTER}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 1000 * 20);
 };
